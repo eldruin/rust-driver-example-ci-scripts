b9e4c1d2517f7f9e1fd2085b37c1d4528c40f008
Diego Barrios Romero <eldruin@gmail.com>
2019-02-17 11:55:57 +0100

    Support using git version of drivers

--- script.sh
+++ script.sh
@@ -1,6 +1,14 @@
 set -exo pipefail
 
 main() {
+
+    if [ ! -z "$GIT_DRIVERS" ]; then
+        for DRIVER in $GIT_DRIVERS;
+        do
+            sed -i "s/$DRIVER\s*=\s*\"[0-9|.]*\"/$DRIVER = \{ git = \"https:\/\/github.com\/eldruin\/$DRIVER-rs\" \}/g" Cargo.toml
+        done
+    fi
+
     export CARGO_OPTIONS="$CARGO_OPTIONS --target $TARGET"
 
     if [ ! -z $FEATURES ]; then
